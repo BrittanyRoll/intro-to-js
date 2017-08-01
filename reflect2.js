9f0038714e3fb9f7d0cd7e8d5b39aab43a72f54b
BrittanyRoll <artemismoon2015@gmail.com>
2017-08-01 11:54:29 -0400

    js file

---
+++ reflect2.js
@@ -0,0 +1,13 @@
+//window.alert("This is Brittany's Page");
+
+function changeColor(color) {
+  document.body.style.background = color;
+}
+
+function openNav() {
+  document.getElementById("dropcontent").style.visibility = "visible";
+}
+
+function closeNav() {
+  document.getElementById("dropcontent").style.visibility = "hidden";
+}
